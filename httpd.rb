1954bd32272b98d138328012f2434cc1b71e7924
Glen Huang <curvedmark@gmail.com>
2014-03-18 22:23:11 +0800

    Apache HTTPD 2.4.9
    - let http decide default mpm
    - apr in osx 10.9.x is broken, use custom version instead
    - enable-mods-shared=all should cover most feature enablings
    - prevent apachectl from going daemon

--- httpd.rb
+++ httpd.rb
@@ -2,39 +2,31 @@ require 'formula'
 
 class Httpd < Formula
   homepage 'http://httpd.apache.org/'
-  url 'http://www.apache.org/dist/httpd/httpd-2.2.26.tar.bz2'
-  sha1 'ecfa7dab239ef177668ad1d5cf9d03c4602607b8'
+  url 'http://apache.tradebit.com/pub/httpd/httpd-2.4.9.tar.bz2'
+  sha1 '646aedbf59519e914c424b3a85d846bf189be3f4'
 
-  skip_clean :la
+  depends_on 'apr'
+  depends_on 'apr-util'
+  depends_on 'pcre'
 
   def install
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--prefix=#{prefix}",
-                          "--mandir=#{man}",
-                          "--localstatedir=#{var}/apache2",
-                          "--sysconfdir=#{etc}/apache2",
-                          "--enable-layout=GNU",
-                          "--enable-mods-shared=all",
-                          "--with-ssl=/usr",
-                          "--with-mpm=prefork",
-                          "--disable-unique-id",
-                          "--enable-ssl",
-                          "--enable-dav",
-                          "--enable-cache",
-                          "--enable-proxy",
-                          "--enable-logio",
-                          "--enable-deflate",
-                          "--with-included-apr",
-                          "--enable-cgi",
-                          "--enable-cgid",
-                          "--enable-suexec",
-                          "--enable-rewrite"
+    # install custom layout
+    File.open('config.layout', 'w') { |f| f.write(httpd_layout) };
+
+    system "./configure",
+      "--enable-layout=Homebrew",
+      "--enable-mods-shared=all",
+      "--with-apr=#{Formula.factory('apr').opt_prefix}",
+      "--with-apr-util=#{Formula.factory('apr-util').opt_prefix}",
+      "--with-pcre=#{Formula.factory('pcre').opt_prefix}"
+
     system "make"
     system "make install"
     (var/'apache2/log').mkpath
     (var/'apache2/run').mkpath
   end
+
+  plist_options :manual => 'apachectl'
 
   def plist; <<-EOS.undent
     <?xml version="1.0" encoding="UTF-8"?>
@@ -46,12 +38,39 @@ class Httpd < Formula
       <key>ProgramArguments</key>
       <array>
         <string>#{opt_prefix}/sbin/apachectl</string>
-        <string>start</string>
+        <string>-DFOREGROUND</string>
       </array>
       <key>RunAtLoad</key>
       <true/>
     </dict>
     </plist>
     EOS
+  end
+
+  def httpd_layout
+    return <<-EOS.undent
+      <Layout Homebrew>
+          prefix:        #{prefix}
+          exec_prefix:   ${prefix}
+          bindir:        ${exec_prefix}/bin
+          sbindir:       ${exec_prefix}/bin
+          libdir:        ${exec_prefix}/lib
+          libexecdir:    ${exec_prefix}/libexec
+          mandir:        #{man}
+          sysconfdir:    #{etc}/apache2
+          datadir:       #{var}/www
+          installbuilddir: ${datadir}/build
+          errordir:      ${datadir}/error
+          iconsdir:      ${datadir}/icons
+          htdocsdir:     ${datadir}/htdocs
+          manualdir:     ${datadir}/manual
+          cgidir:        #{var}/apache2/cgi-bin
+          includedir:    ${prefix}/include/apache2
+          localstatedir: #{var}/apache2
+          runtimedir:    #{var}/run/apache2
+          logfiledir:    #{var}/log/apache2
+          proxycachedir: ${localstatedir}/proxy
+      </Layout>
+      EOS
   end
 end
